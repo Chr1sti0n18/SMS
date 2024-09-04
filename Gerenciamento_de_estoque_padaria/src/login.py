89d71ad5e4820ee69e6ac067ac25851c72eae70f
ChrisRib <chrisribgama3@gmail.com>
2024-09-04 16:10:33 -0300

    Adicionando pequena alteração para diminuir código

--- Gerenciamento_de_estoque_padaria/src/login.py
+++ Gerenciamento_de_estoque_padaria/src/login.py
@@ -10,12 +10,12 @@ def LoginForm():
     main_window = CTk()
     main_window.title("Login")
     main_window.geometry("640x480+620+275")
-    main_window._set_appearance_mode("light")
+    main_window._set_appearance_mode("system")
     main_window.resizable(False,False)   
 
     # Montando a estrutura da Tela
     title_label = CTkLabel(main_window,text="Ki Pães",bg_color="#756645",text_color="#F4EE82",
-                        font=("Montserrat", 52, "bold"),justify=CENTER,padx=400,height=100)
+                        font=("Montserrat", 52, "bold"),padx=400,height=100)
     title_label.pack()
     user_entry = CTkEntry(main_window,bg_color="white",font=("Montserrat", 22),placeholder_text="Seu usuário")
     user_entry.pack(padx=10,pady=60)
