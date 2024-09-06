c5b21ee8273e44d39ccf0aac3ef95c5418257bd3
Joaovict07 <joaovictr204@gmail.com>
2024-09-06 20:04:19 -0300

    ajustes de espacamento

--- Gerenciamento_de_estoque_padaria/src/produtos/Read_products.py
+++ Gerenciamento_de_estoque_padaria/src/produtos/Read_products.py
@@ -14,14 +14,21 @@ class Read:
         
         # Carregando a logo
         logo = ctk.CTkImage(light_image=Image.open("Gerenciamento_de_estoque_padaria/src/assets/logo.png"), 
-                            dark_image=Image.open("Gerenciamento_de_estoque_padaria/src/assets/logo.png"), size=(75, 75))    
+                            dark_image=Image.open("Gerenciamento_de_estoque_padaria/src/assets/logo.png"), size=(90, 90))    
         
-        
+        # Criando frame principal
         self.frame1 = ctk.CTkFrame(self.main_window)
         self.frame1.pack(side="top", fill="y", expand = False, ipadx = 1280)
+        self.inner_frame1 = ctk.CTkFrame(self.frame1)
+        self.inner_frame1.pack(fill="both") 
         
-        self.head = ctk.CTkLabel(self.frame1, text="Produtos", font=("MONTSERRAT", 22, "bold", anchor="center"), 
-                                 fg_color="#FFC07E", text_color="#554131",height=80, image=logo, compound="left", padx = 350)
+         # Texto da frame principal
+        self.head = ctk.CTkLabel(self.inner_frame1, text="Produtos", font=("MONTSERRAT", 22, "bold"), 
+                                 fg_color="#FFC07E", text_color="#554131",height=90, width=1280, anchor="center")
+        
+        self.image_label = ctk.CTkLabel(self.inner_frame1, image=logo, text="")
+        self.image_label.pack(side="left", anchor="w")  # Sem padding, a imagem fica na extrema esquerda
+        
         self.head.pack(fill="both") 
         
         # Criando a tabela
